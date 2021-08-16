30054d99b1ef69acab4bc47ef259660d8c34ac44
FreddySG <freddy.salasxp@gmail.com>
2021-08-15 22:37:18 -0500

    Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
 import { XyzComponent } from './xyz/xyz.component';
 
 @NgModule({
@@ -14,6 +15,7 @@ import { XyzComponent } from './xyz/xyz.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     FormsModule
   ],
